ff99596e1849ab5ee1a42cce810c6008fb81536f
Taisei Suzuki <iseita31kamekichi@gmail.com>
2025-01-12 18:54:57 +0900

    remake

--- test/test.bash
+++ test/test.bash
@@ -3,18 +3,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 set -e  # エラー発生時にスクリプトを停止
-dir=~
+dir="$HOME"
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws
+cd "$dir/ros2_ws"
 colcon build
-source $dir/.bashrc
-
+source /opt/ros/rolling/setup.bash
+source "$dir/ros2_ws/install/setup.bash"
 
 timeout 65 ros2 launch mypkg btc_listen.launch.py > /tmp/mypkg.log &
+LAUNCH_PID=$!
 
+# プロセスが終了するまで待機
+wait $LAUNCH_PID || echo "Launch process terminated with a timeout."
 
-
-cat /tmp/mypkg.log | grep -E btc_price
-
+# ログファイルの内容を確認
+if [[ -s /tmp/mypkg.log ]]; then
+  grep -E btc_price /tmp/mypkg.log || echo "No matching lines found."
+else
+  echo "Log file is empty or does not exist."
+fi
 
