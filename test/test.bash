c90c24c833010b7138e778b9df189e93e2e1ecbc
Taisei Suzuki <iseita31kamekichi@gmail.com>
2025-01-18 18:35:17 +0900

    remake

--- test/test.bash
+++ test/test.bash
@@ -20,12 +20,6 @@ sleep 15
 
 # トピックを監視し、出力をログに記録
 timeout 65 ros2 topic echo /btc_price --qos-profile sensor_data> /tmp/mypkg.log
-if [ -s /tmp/mypkg.log ]; then
-    echo "データ取得成功："
-    cat /tmp/mypkg.log
-else
-    echo "エラー: トピックデータが取得できませんでした。"
-fi
 
 #ログの内容を表示
 cat /tmp/mypkg.log
