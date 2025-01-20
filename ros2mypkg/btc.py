5dd1f07c6a2455a50341940c36dc915b1d52bd3a
Taisei Suzuki <iseita31kamekichi@gmail.com>
2025-01-20 13:08:52 +0900

    remake

--- ros2mypkg/btc.py
+++ ros2mypkg/btc.py
@@ -1,22 +1,25 @@
 # SPDX-FileCopyrightText: 2024 Taisei Suzuki
 # SPDX-License-Identifier: BSD-3-Clause
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node  # Nodeクラスをインポート
 from std_msgs.msg import String
 import yfinance as yf
 from datetime import datetime
 
-
 class BTCPublisher(Node):
     def __init__(self):
         super().__init__('btc_publisher')
-        self.publisher_ = self.create_publisher(String, 'btc_price', 10)
+        self.publisher_ = self.create_publisher(
+            String,
+            'btc_price',
+            qos_profile=rclpy.qos.QoSPresetProfiles.SENSOR_DATA.value,
+        )
         self.create_timer(60.0, self.publish_price)
 
     def publish_price(self):
         try:
             ticker = yf.Ticker("BTC-USD")
-            current_price = ticker.history(period="1d")['Close'][0]  # 最新の終値を取得
+            current_price = ticker.history(period="1d")['Close'][0]
             now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             message = f"{now}, ${current_price:.2f}"
             self.publisher_.publish(String(data=message))
